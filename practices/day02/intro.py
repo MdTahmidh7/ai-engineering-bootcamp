114f5ed3f8407468bdf318dd702aca40ff54bc4d
Tahmid <abdullah.tahmid@bracits.com>
2026-07-01 11:32:21 +0600

    Day2 Python Basic Started with code and added intro.py

---
+++ practices/day02/intro.py
@@ -0,0 +1,14 @@
+print("Hello AI engineers")
+
+name = input("Enter your name: ")
+print(f"Nice to meet you, {name}!")
+
+age = input("What is your age?")
+intAge = int(age)
+
+if intAge < 18:
+    print("You are a minor.")
+else:
+    print("You are an adult.")
+
+print(" You can exit the program by pressing Ctrl+C or Ctrl+D")
